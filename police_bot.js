f191679b9d9cba4077600cdcb92d0393b0ee5db9
Nuko <nuko973663@gmail.com>
2022-02-20 00:06:41 +0900

    update

--- police_bot.js
+++ police_bot.js
@@ -2,9 +2,10 @@
  * bot.js
  */
 
-"use strict";
-const TwitterApi = require("twitter-api-v2").default;
-const config = require("./twitter.json");
+import { TwitterApi } from "twitter-api-v2";
+import { sleep } from "./sleep.js";
+import fs from "fs";
+const config = JSON.parse(fs.readFileSync("./twitter.json", "utf8"));
 
 const client = new TwitterApi({
   appKey: config.keys.api_key,
@@ -13,23 +14,54 @@ const client = new TwitterApi({
   accessSecret: config.keys.access_token_secret,
 });
 
-//const client = new TwitterApi(config.keys.bearer_token);
-
 const getBaits = () => {
   return config.baits[Math.floor(Math.random() * config.baits.length)];
 };
 
+const getMyId = async () => {
+  const meUser = await client.v2.me();
+  return meUser.data;
+};
+
 const main = async () => {
-  //const user = await client.v2.userByUsername("nuko973663");
-  await client.v1.tweet("Hello, this is a test.");
-  //const meUser = await client.v2.me();
-  //onsole.log(meUser);
-  return;
-  let { data: createdTweet } = await client.v2.tweet(getBaits());
+  const me = await getMyId();
+  console.log(me.id, " : ", me.name);
+
+  let { data: createdTweet } = await client.v2.tweet(
+    getBaits() + " " + Date.now()
+  );
   console.log("Tweet", createdTweet.id, ":", createdTweet.text);
 
-  //await twitterClient.v1.tweet("Hello, this is a test.");
-  // You can upload media easily!
+  console.log("wait for reply");
+  await sleep(10000);
+
+  const tweetsOfMe = await client.v2.userMentionTimeline(me.id, {
+    expansions: ["author_id", "in_reply_to_user_id"],
+  });
+  for (const fetchedTweet of tweetsOfMe) {
+    if (fetchedTweet.text.indexOf("https://t.co/") > 0) {
+      const spammer = await client.v2.user(fetchedTweet.author_id, {
+        "tweet.fields": ["id", "text"],
+      });
+
+      console.log("Detect Spam");
+      console.log(spammer);
+      console.log(fetchedTweet);
+
+      try {
+        const res = await client.v1.post(
+          "users/report_spam.json",
+          {
+            user_id: spammer.id,
+          },
+          { forceBodyMode: "json" }
+        );
+
+        console.log(res);
+        console.log("@" + spammer.username + " を通報しました");
+      } catch (e) {}
+    }
+  }
 };
 
 main();
